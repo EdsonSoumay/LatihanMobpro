d881eaf067eb2345ac950fd6ad5f2e7d47e759ce
EdsonSoumay <s21810088@student.unklab.ac.id>
2021-04-18 14:54:48 +0800

    exercise 7

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View } from 'react-native'
 import HomeScreen from './pages/userlist/index'
 import Registrasi from './pages/registrasi/index'
 import List from './pages/searchJSON/index'
+import InputData from './inputData/inputData.js'
 const App = () => {
   return (
     <View>
        {/* <HomeScreen/> */}
        {/* <Registrasi/>  */}
-      <List/>
+      {/* <List/> */}
+      <InputData/>
     </View>
   )
 }
