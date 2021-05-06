1aa3006234d2cb157a4b590119dc94353dce9023
EdsonSoumay <s21810088@student.unklab.ac.id>
2021-05-06 15:49:30 +0800

    Kevin Likuayang

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import HomeScreen from './pages/userlist/index'
-import Registrasi from './pages/registrasi/index'
-import List from './pages/searchJSON/index'
-import InputData from './inputData/inputData.js'
+
 const App = () => {
   return (
     <View>
-       {/* <HomeScreen/> */}
-       {/* <Registrasi/>  */}
-      {/* <List/> */}
-      <InputData/>
+      <Text> Unklab Hymnal</Text>
     </View>
   )
 }
